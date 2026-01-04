bac73571b9eb1adb2a42fcc0992d82e7d98acf78
Aditya Prakash <prakashaditya2709@gmail.com>
2026-01-04 09:40:10 +0530

    Added CatalanNumber.py, __init.py and Basics.py

---
+++ basics/Basics.py
@@ -0,0 +1,25 @@
+# a=input()
+# l=[]
+#
+# for i in a:
+#     l.append(i)
+# for i in range(0,len(l)):
+#     print(l[i])
+
+
+# a= int(input("Enter a number: "))
+# b= int(input("Enter another number: "))
+# avg = (a+b)/2
+# print(int(avg))
+
+
+# a = int(input("Enter a number: "))
+# pow = a**2
+# print(pow)
+
+
+name = "Aditya"
+sl = name[0:3]
+print(sl)
+sl = name[3:5]
+print(sl)
