de685174677637577b016e35c06550a0faba38e6
Aditya Prakash <prakashaditya2709@gmail.com>
2026-01-25 20:09:36 +0530

    Added ResumeSkillGapAnalyzer project

---
+++ numpy_operations/Arrays.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr = np.array([1,2,3])  # faster than list
+np.zeros(5)
+np.ones((2,3))
+print(arr)
+print(np.arange(1,3,6))
+print(np.linspace(0,1,5))
+print(np.random.rand(3,3))
+print(np.random.randint(0,10,5))
